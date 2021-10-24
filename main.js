4506f1fdf6a03b9bc997a0787807a344ea3f9ccb
jamcheri <james.d.cherian@gmail.com>
2021-10-24 15:23:07 -0700

    cleaned up UI, added some basic logic for the calc

--- main.js
+++ main.js
@@ -20,25 +20,171 @@ function divide(numOne, numTwo) {
 };
 
 
+
+let operator = "";
+let operatedCycle = false;
+
+
 //operate function that takes in the userInput and performs the calculation
 function operate(operator, numOne, numTwo) {
-    let operator = userInput;
+    //let operator = userInput;
     let operateResult = 0;
 
-    if (operator = "add") {
+    if (operator === "add") {
         operateResult = add(numOne, numTwo);
-    } else if (operator = "subtract") {
+    } else if (operator === "subtract") {
         operateResult = subtract(numOne, numTwo);
-    } else if (operator = "multiply") {
+    } else if (operator === "multiply") {
         operateResult = multiply(numOne, numTwo);
-    } else if (operator = "divide") {
+    } else if (operator === "divide") {
         operateResult = divide(numOne, numTwo);
     }
+    document.getElementById("display").innerHTML=operateResult; //send code to display
+    console.log("operateResult is " + operateResult);
+    console.log("operatorClicked is " + operatorClicked);
 
+    operatedCycle = true;
+    numOne = operateResult;
+    numTwo = "";
+    value = "";
     return operateResult;
-}
+};
+
+//setup each digit button
+
+let value = "";
+let operatorClicked = false;
 
 
+
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        if (operatorClicked === false && operatedCycle === false) {
+            if (value.includes(".") && button.id === "period") {
+                console.log(value);
+            } else if (button.id === "zero" && value.charAt(0) === "0" && !value.includes(".")) {
+                console.log(value);
+            } else if (button.classList.contains("digits")) {
+                value = value.toString();
+                value += button.textContent;
+                console.log(value);
+            } else if (button.classList.contains("operators") && value === "") {
+                console.log(value);
+            } else if (value != "" && value != "." && button.id === "equals") {
+                console.log(value);
+                
+    
+            } else if (button.classList.contains("operators")) {
+                operatorClicked = true;
+                console.log(operatorClicked);
+                operator = button.id;
+                console.log(operator);
+                numOne = parseFloat(value);
+                console.log("numOne is " + numOne);
+                value = "";
+                return;
+            } else {
+                return;
+            }
+        }
+        
+        
+        
+        else if (operatedCycle === false) {
+            if (value.includes(".") && button.id === "period") {
+                console.log(value);
+            } else if (button.id === "zero" && value.charAt(0) === "0" && !value.includes(".")) {
+                console.log(value);
+            } else if (button.classList.contains("digits")) {
+                value = value.toString();
+                value += button.textContent;
+                console.log(value);
+            } else if (button.classList.contains("operators") && value === "") {
+                console.log(value);
+            } else if (value != "" && value != "." && button.id === "equals") {
+                console.log(value);
+                numTwo = parseFloat(value);
+                console.log("numOne is " + numOne);
+                console.log("numTwo is " + numTwo);
+                console.log("operator is " + operator);
+                return operate(operator, numOne, numTwo);
+            } else if (button.classList.contains("operators")) {
+                operatorClicked = true;
+                console.log(operatorClicked);
+                operator = button.id;
+                console.log(operator);
+                numTwo = parseFloat(value);
+                console.log(numOne);
+                value = "";
+                return;
+            } else {
+                return;
+            }
+        } 
+        
+        else { //this is 
+            console.log("value is " + value);
+            console.log("numOne is " + numOne);
+            console.log("numTwo is " + numTwo);
+            
+            if (value.includes(".") && button.id === "period") {
+                console.log(value);
+            } else if (button.id === "zero" && value.charAt(0) === "0" && !value.includes(".")) {
+                console.log(value);
+            } else if (button.classList.contains("digits")) {
+                value = value.toString();
+                value += button.textContent;
+                console.log(value);
+            } else if (button.classList.contains("operators") && value === "") {
+                console.log(value);
+            } else if (value != "" && value != "." && button.id === "equals") {
+                console.log(value);
+                numTwo = parseFloat(value); 
+            } else if (button.classList.contains("operators")) {
+                operatorClicked = true;
+                console.log(operatorClicked);
+                operator = button.id;
+                console.log(operator);
+                numTwo = parseFloat(value);
+                console.log(numOne);
+                value = "";
+                return;
+            }
+        };
+    
+     
+    });
+
+
+});
+
+
+
+
+
+
+//const digitButton = document.querySelectorAll(".digits");
+
+
+
+
+//each time digit is pressed, add to a string
+
+
+
+/*
+const button = document.querySelector("#button");       //button to reset 
+button.addEventListener('click', () => {
+    userInput = prompt("What dimensions should the new grid be? X number by X number:");
+
+    while (userInput > 100) {                           //don't let grid get larger than 100x100
+        userInput = prompt("Grid dimension cannot be larger than 100, please enter a lower dimension:")
+    }
+
+    makeGrid(userInput);
+});*/
 
 
 
